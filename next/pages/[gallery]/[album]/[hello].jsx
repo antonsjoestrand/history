d1c71e7a4f656ba43db03ce492fdc15b584ec57d
antonsjoestrand <85382660+antonsjoestrand@users.noreply.github.com>
2022-01-28 15:50:46 -0800

    Corrected lint issues

--- next/pages/[gallery]/[album]/[hello].jsx
+++ next/pages/[gallery]/[album]/[hello].jsx
@@ -1,22 +1,23 @@
-import Head from "next/head";
-import { useEffect, useState } from 'react';
-import styled from "styled-components";
+/* global fetch */
+
+import Head from 'next/head'
+import { useEffect, useState } from 'react'
+import styled from 'styled-components'
 
 function Hello() {
   const HelloWorld = styled.h1`
     font-size: 6rem;
     color: crimson;
     text-align: center;
-  `;
+  `
 
-  const [photos, setPhotos] = useState([]);
+  const [photos, setPhotos] = useState([])
 
-  useEffect(async() => {
-    const response = await fetch("http://localhost:3005/api/flickr");
-    const result = await response.json();
-    setPhotos(result.photos);
-  }, []);
-
+  useEffect(async () => {
+    const response = await fetch('http://localhost:3005/api/flickr')
+    const result = await response.json()
+    setPhotos(result.photos)
+  }, [])
 
   return (
     <div>
@@ -28,12 +29,12 @@ function Hello() {
         <HelloWorld>Hello World!</HelloWorld>
         <ul>
           {photos.map((photo) => (
-            <img src={photo.path} alt={photo.alt} />
+            <img src={photo.path} alt={photo.alt} width="200" height="140" />
           ))}
         </ul>
       </h1>
     </div>
-  );
+  )
 }
 
-export default Hello;
+export default Hello
